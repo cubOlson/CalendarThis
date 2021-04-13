f55e49d303387ff490d872038fca0dc71d1d9a01
Heriberto Montoya <hmontoya3821@gmail.com>
2021-04-13 18:47:41 -0500

    trying to make a validation form and handle sumbit

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
 import os
 import psycopg2
 from app.forms import AppointmentForm
+from datetime import datetime
 
 bp = Blueprint('main', __name__, '/')
 
@@ -12,9 +13,30 @@ CONNECTION_PARAMETERS = {
     "host": os.environ.get("DB_HOST"),
 }
 
-@bp.route('/')
+
+
+@bp.route('/', methods=["POST", "GET"])
 def main():
     form = AppointmentForm()
+    params = {
+    'name': form.name.data,
+    'start_datetime': datetime.combine(form.start_date.data, form.start_time.data),
+    'end_datetime': datetime.combine(form.end_date.data, form.end_time.data),
+    'description': form.description.data,
+    'private': form.private.data
+    }
+    if form.validate():
+        print('am i in')
+        with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute('''
+                        INSERT INTO appointments(name, start_datetime, end_datetime, description, private)
+                        VALUES (%(name)s, %(start_datetime)s, %(end_datetime)s, %(description)s, %(private)s);
+                    ''',
+                    params
+                    # {'name': params.name,'start_datetime': params.start_datetime, 'end_datetime': params.end_datetime, 'description': params.description, 'private': params.private},
+                    )
+                return redirect('/')
     with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
         with conn.cursor() as cursor:
             cursor.execute('''
@@ -23,5 +45,6 @@ def main():
                 ORDER BY start_datetime
                 '''
             )
+
             rows = cursor.fetchall()
     return render_template('main.html', rows=rows, form=form)
